a75f05482a9c8908ea039c456fb693a4514318e8
Simon Leung <leungs@us.ibm.com>
2014-11-01 16:08:48 -0700

    bosh-lite login choice

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type IBM_Bluemix struct{}
 
 func (c *IBM_Bluemix) Run(args []string, reply *bool) error {
 	if args[0] == "bluemix-login" {
-		bluemix_login()
+		login()
 	} else if args[0] == "bluemix-push" {
 		bluemix_push()
 	}
@@ -34,21 +34,15 @@ func (c *IBM_Bluemix) GetCommands() []plugin.Command {
 	}
 }
 
-func bluemix_login() {
-	var host, user string
-	fmt.Print("login api: (https://api.ng.bluemix.net)")
-	fmt.Scanf("%s", &host)
-	if host == "" {
-		host = "https://api.ng.bluemix.net"
+func login() {
+	var choice int
+	fmt.Printf("\nSelect a target:\n1. Local Bosh-lite\n2. Bluemix\n\nTarget> ")
+	fmt.Scanf("%d", &choice)
+	if choice == 1 {
+		boshlite_login()
+	} else if choice == 2 {
+		bluemixLogin()
 	}
-
-	fmt.Print("login for bluemix: ")
-	fmt.Scanf("%s", &user)
-
-	fmt.Print("password for bluemix: ")
-	pass := gopass.GetPasswd()
-
-	plugin.CliCommand("login", "-a", host, "-u", user, "-p", string(pass), "-o", "cfplayground", "-s", "dev")
 }
 
 func bluemix_push() {
@@ -92,4 +86,26 @@ func pushApp(outDir string) error {
 	fmt.Println("Pushing app to bluemix...")
 	_, err := plugin.CliCommand("push", "-p", outDir, "cli_plugin_dora")
 	return err
+}
+
+func bluemixLogin() {
+	var host, user string
+	fmt.Print("login api: (https://api.ng.bluemix.net)")
+	fmt.Scanf("%s", &host)
+	if host == "" {
+		host = "https://api.ng.bluemix.net"
+	}
+
+	fmt.Print("login for bluemix: ")
+	fmt.Scanf("%s", &user)
+
+	fmt.Print("password for bluemix: ")
+	pass := gopass.GetPasswd()
+
+	plugin.CliCommand("login", "-a", host, "-u", user, "-p", string(pass), "-o", "cf_playground", "-s", "dev")
+}
+
+func boshlite_login() {
+	fmt.Print("loging into bosh-lite...")
+	plugin.CliCommand("login", "-a", "https://api.10.244.0.34.xip.io", "-u", "admin", "-p", "admin", "--skip-ssl-validation")
 }
